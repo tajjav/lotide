ecf0f4b456d3f37be2b10958d9760bb2537eedf8
tajjav <taj.jav@hotmail.com>
2023-09-17 11:00:21 -0600

    added assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+//Test Code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
