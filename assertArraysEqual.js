89af5706c387960b476c69b8404444e936c664c0
tajjav <taj.jav@hotmail.com>
2023-10-04 21:44:04 -0600

    refactored assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,10 @@
+const eqArrays = require('./eqArrays');
+
+/**
+ * assertArraysEqual function definition, it prints message to console that whether two arrays are equal or not.
+ * @param {Array} ckArray1 
+ * @param {Array} ckArray2 
+ */
 const assertArraysEqual = function (ckArray1, ckArray2) {
   let result = eqArrays(ckArray1, ckArray2);
   if (result)
@@ -6,21 +13,5 @@ const assertArraysEqual = function (ckArray1, ckArray2) {
     console.log("Two arrays are not equal");
 }
 
+module.exports = assertArraysEqual;
 
-const eqArrays = function (chkArray1, chkArray2) {
-
-  if (chkArray1.length !== chkArray2.length)
-    return false;
-
-  for (let i = 0; i < chkArray1.length; i++) {
-    if (chkArray1[i] !== chkArray2[i])
-      return false;
-  }
-
-  return true;
-}
-
-
-//Test
-assertArraysEqual([1, 2, 3], [1, 2, 3])
-assertArraysEqual([1, 2, 3, 4], [4, 3, 2, 1])
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,7 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+
+
+//Test
+assertArraysEqual([1, 2, 3], [1, 2, 3])
+assertArraysEqual([1, 2, 3, 4], [4, 3, 2, 1])
